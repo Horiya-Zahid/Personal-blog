d8f1ef0de28104e655adbd7552249320c109dbed
unknown <horiyazahid09@gmail.com>
2024-12-31 19:22:54 +0500

    Commented out the StudioPage component code for debugging

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -8,13 +8,27 @@
  */
 
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+// import { NextStudio } from 'next-sanity/studio'
+// import config from '../../../../sanity.config'
 
-export const dynamic = 'force-static'
+// export const dynamic = 'force-static'
 
-export { metadata, viewport } from 'next-sanity/studio'
+// export { metadata, viewport } from 'next-sanity/studio'
 
-export default function StudioPage() {
-  return <NextStudio config={config} />
-}
+// export default function StudioPage() {
+//   return <NextStudio config={config} />
+// }
+
+
+// import { NextStudio } from 'next-sanity/studio';
+
+// import config from '../../../../sanity.config';
+
+// export const dynamic = 'force-static';
+
+// export { metadata, viewport } from 'next-sanity/studio';
+
+// export default function StudioPage() {
+//   return <NextStudio config={config} /> as unknown as JSX.Element;
+// }
+
